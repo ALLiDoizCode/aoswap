2939be2aa7801eb02472ba6be487e9248513dfa9
Jonathan Green <dev.jonathan.green@gmail.com>
2024-04-10 13:26:05 -0400

    WIP

--- mods/Token.lua
+++ mods/Token.lua
@@ -7,6 +7,7 @@ Mod = {}
 function Mod.info(msg)
   ao.send({
     Target = msg.From,
+    Action = "Response",
     Name = Name,
     Ticker = Ticker,
     Logo = Logo,
@@ -31,36 +32,35 @@ function Mod.balance(msg)
     Balance = bal,
     Ticker = Ticker,
     Account = msg.Tags.Target or msg.From,
-    Data = bal,
     Nonce = msg.Nonce,
   })
 end
 
 function Mod.balances(msg)
-  ao.send({ Target = msg.From, Data = json.encode(Balances), Nonce = msg.Nonce, })
+  ao.send({ Target = msg.From, Data = json.encode(Balances), Action = 'Response', Nonce = msg.Nonce, })
 end
 
 function Mod.allowance(msg)
   assert(type(msg.Spender) == 'string', 'Spender is required!')
-  local bal = '0'
+  local allowance = '0'
   -- If no Target is provided, then return the Senders allowance
   if (msg.Tags.Target and Allowances[msg.Tags.Target][msg.Spender]) then
-    bal = Allowances[msg.Tags.Target][msg.Spender]
+    allowance = Allowances[msg.Tags.Target][msg.Spender]
   elseif Allowances[msg.From][msg.Spender] then
-    bal = Allowances[msg.From][msg.Spender]
+    allowance = Allowances[msg.From][msg.Spender]
   end
   ao.send({
     Target = msg.From,
-    Balance = bal,
+    Action = 'Response',
     Ticker = Ticker,
     Account = msg.Tags.Target or msg.From,
-    Data = bal,
+    Allowance = allowance,
     Nonce = msg.Nonce,
   })
 end
 
 function Mod.allowances(msg)
-  ao.send({ Target = msg.From, Data = json.encode(Allowances), Nonce = msg.Nonce, })
+  ao.send({ Target = msg.From, Data = json.encode(Allowances), Action = 'Response', Nonce = msg.Nonce, })
 end
 
 function Mod.transfer(msg)
@@ -76,36 +76,20 @@ function Mod.transfer(msg)
   if bint.__le(qty, balance) then
     Balances[msg.From] = tostring(bint.__sub(balance, qty))
     Balances[msg.Recipient] = tostring(bint.__add(Balances[msg.Recipient], qty))
-
-    --[[
-         Only send the notifications to the Sender and Recipient
-         if the Cast tag is not set on the Transfer message
-       ]]
-    --
-    if not msg.Cast then
-      -- Send Debit-Notice to the Sender
-      ao.send({
-        Target = msg.From,
-        Action = 'Debit-Notice',
-        Recipient = msg.Recipient,
-        Quantity = tostring(qty),
-        Data = Colors.gray ..
-            "You transferred " ..
-            Colors.blue .. msg.Quantity .. Colors.gray .. " to " .. Colors.green .. msg.Recipient .. Colors.reset,
-        Nonce = msg.Nonce,
-      })
-      -- Send Credit-Notice to the Recipient
-      ao.send({
-        Target = msg.Recipient,
-        Action = 'Credit-Notice',
-        Sender = msg.From,
-        Quantity = tostring(qty),
-        Data = Colors.gray ..
-            "You received " ..
-            Colors.blue .. msg.Quantity .. Colors.gray .. " from " .. Colors.green .. msg.From .. Colors.reset,
-        Nonce = msg.Nonce,
-      })
-    end
+    ao.send({
+      Target = msg.From,
+      Action = 'Response',
+      Recipient = msg.Recipient,
+      Quantity = tostring(qty),
+      Nonce = msg.Nonce,
+    })
+    ao.send({
+      Target = msg.Recipient,
+      Action = 'Response',
+      Sender = msg.From,
+      Quantity = tostring(qty),
+      Nonce = msg.Nonce,
+    })
   else
     ao.send({
       Target = msg.From,
@@ -136,36 +120,21 @@ function Mod.transferFrom(msg)
       Balances[msg.OwnerId] = tostring(bint.__sub(balance, qty))
       Allowances[msg.OwnerId][msg.from] = tostring(bint.__sub(allowance, qty))
       Balances[msg.Recipient] = tostring(bint.__add(Balances[msg.Recipient], qty))
-
-      --[[
-          Only send the notifications to the Owner and Recipient
-          if the Cast tag is not set on the TransferFrom message
-        ]]
-      --
-      if not msg.Cast then
-        -- Send Debit-Notice to the Owner
-        ao.send({
-          Target = msg.OwnerId,
-          Action = 'Response',
-          Recipient = msg.Recipient,
-          Quantity = tostring(qty),
-          Data = Colors.gray ..
-              "You transferred " ..
-              Colors.blue .. msg.Quantity .. Colors.gray .. " to " .. Colors.green .. msg.Recipient .. Colors.reset,
-          Nonce = msg.Nonce,
-        })
-        -- Send Credit-Notice to the Recipient
-        ao.send({
-          Target = msg.Recipient,
-          Action = 'Response',
-          Sender = msg.OwnerId,
-          Quantity = tostring(qty),
-          Data = Colors.gray ..
-              "You received " ..
-              Colors.blue .. msg.Quantity .. Colors.gray .. " from " .. Colors.green .. msg.From .. Colors.reset,
-          Nonce = msg.Nonce,
-        })
-      end
+      ao.send({
+        Target = msg.OwnerId,
+        Action = 'Response',
+        Recipient = msg.Recipient,
+        Quantity = tostring(qty),
+        Nonce = msg.Nonce,
+      })
+      -- Send Credit-Notice to the Recipient
+      ao.send({
+        Target = msg.Recipient,
+        Action = 'Response',
+        Sender = msg.OwnerId,
+        Quantity = tostring(qty),
+        Nonce = msg.Nonce,
+      })
     else
       ao.send({
         Target = msg.from,
@@ -194,6 +163,11 @@ function Mod.approve(msg)
   assert(bint.__lt(0, bint(msg.Quantity)), 'Quantity must be greater than 0')
   if not Allowances[msg.From] then Allowances[msg.From] = {} end
   Allowances[msg.From][msg.Spender] = msg.Quantity
+  ao.send({
+    Target = msg.From,
+    Action = 'Response',
+    Nonce = msg.Nonce,
+  })
 end
 
 function Mod.mint(msg)
@@ -201,25 +175,14 @@ function Mod.mint(msg)
   assert(type(msg.Quantity) == 'string', 'Quantity is required!')
   assert(bint.__lt(0, msg.Quantity), 'Quantity must be greater than zero!')
 
-  if not Balances[ao.id] then Balances[ao.id] = "0" end
-
-  if msg.From == ao.id then
-    -- Add tokens to the token pool, according to Quantity
-    Balances[msg.From] = tostring(bint.__add(Balances[Owner], msg.Quantity))
-    ao.send({
-      Target = msg.From,
-      Data = Colors.gray .. "Successfully minted " .. Colors.blue .. msg.Quantity .. Colors.reset,
-      Nonce = msg.Nonce,
-    })
-  else
-    ao.send({
-      Target = msg.From,
-      Action = 'Mint-Error',
-      ['Message-Id'] = msg.Id,
-      Error = 'Only the Process Owner can mint new ' .. Ticker .. ' tokens!',
-      Nonce = msg.Nonce,
-    })
-  end
+  if not Balances[msg.Recipient] then Balances[msg.Recipient] = "0" end
+  -- Add tokens to the token pool, according to Quantity
+  Balances[msg.Recipient] = tostring(bint.__add(Balances[msg.Recipient], msg.Quantity))
+  ao.send({
+    Target = msg.From,
+    Action = 'Response',
+    Nonce = msg.Nonce,
+  })
 end
 
 function Mod.setMinter(msg)
