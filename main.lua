75ec7035fe2aeb07806e1761f1ba2c208f0eec50
Jonathan Green <dev.jonathan.green@gmail.com>
2024-06-26 16:05:52 -0400

    WIP

--- main.lua
+++ main.lua
@@ -8,42 +8,44 @@ FeeRate = 0.01  -- Fee rate (1% in this example)
 TokenA = 0;
 TokenB = 0;
 IsPump = true;
+IsActive = true;
 
 BondingCurve = 0;
 TokenAProcess = "";
 TokenBProcess = "";
 
 Handlers.add('Init', Handlers.utils.hasMatchingTag('Action', 'Init'), function(msg)
+    assert(IsActive == false,"Pool is already active")
     Init(msg)
 end)
 
 Handlers.add("InitalLiquidity", Handlers.utils.hasMatchingTag('Action', "InitalLiquidity"), function(msg)
-    if IsPump then
-        if (TokenA == 0 and TokenB == 0) then
-            InitalLiquidity(msg.From, Utils.toNumber(msg.amountA), Utils.toNumber(msg.amountB))
-            return
-        else
-            Utils.result(msg.From, 403, "You can't add liquidty to pumps") 
-        end;
-        return
-    end;
+    assert(IsActive == false,"Pool is already active")
+    assert(TokenA == 0 and TokenB == 0,"Token balance is in a bad state")
+    assert(IsActive == false,"Pool is already active")
+    InitalLiquidity(msg.From, Utils.toNumber(msg.amountA), Utils.toNumber(msg.amountB))
+    IsActive = true
 end);
 
 Handlers.add("Add", Handlers.utils.hasMatchingTag('Action', "Add"), function(msg)
+    assert(IsActive,"Pool is already active")
     Add(msg.From, Utils.toNumber(msg.amountA), Utils.toNumber(msg.amountB))
 end);
 
 Handlers.add("Remove", Handlers.utils.hasMatchingTag('Action', "Remove"), function(msg)
+    assert(IsActive,"Pool is already active")
     Remove(msg.From, Utils.toNumber(msg.share))
 end);
 
 Handlers.add("SwapA", Handlers.utils.hasMatchingTag('Action', "SwapA"), function(msg)
+    assert(IsActive,"Pool is already active")
     SwapA(msg.From, Utils.toNumber(msg.amount),Utils.toNumber(msg.slippage));
     local _liquidity = GetLiquidity();
     if _liquidity >= BondingCurve then IsPump = false end
 end);
 
 Handlers.add("SwapB", Handlers.utils.hasMatchingTag('Action', "SwapB"), function(msg)
+    assert(IsActive,"Pool is already active")
     SwapB(msg.From, Utils.toNumber(msg.amount),Utils.toNumber(msg.slippage));
     local _liquidity = GetLiquidity();
     if _liquidity >= BondingCurve then IsPump = false end
